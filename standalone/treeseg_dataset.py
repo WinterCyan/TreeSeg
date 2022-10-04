05298161dc5734075648194a273d7fd8238f8b67
WinterCyan <wintercyan98@gmail.com>
2022-10-05 00:38:11 +0800

    unet

---
+++ standalone/treeseg_dataset.py
@@ -0,0 +1,15 @@
+from torch.utils.data import DataLoader, Dataset
+
+class TreeDataset(Dataset):
+    """load dataset from local files
+
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
