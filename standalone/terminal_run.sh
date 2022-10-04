509f815380e2e5eeef11a6038f1223c9693a6b2b
WinterCyan <wintercyan98@gmail.com>
2022-10-05 01:00:49 +0800

    update sh

--- standalone/terminal_run.sh
+++ standalone/terminal_run.sh
@@ -8,6 +8,7 @@ python3 split_merge_pipeline.py \
     --task preprocess_train \
     --tif_dir /Users/wintercyan/LocalDocuments/treeseg-resource/full_area2/data \
     --area_polygon_dir /Users/wintercyan/LocalDocuments/treeseg-resource/full_area2/data \
+    --area_range 2-3 \
     --interm_png_dir /Users/wintercyan/LocalDocuments/treeseg-resource/full_area2/interm_png
 
 python3 split_merge_pipeline.py \
