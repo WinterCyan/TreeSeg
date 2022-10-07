0217b9aabd5386be292d6e0c9aa389ab7a83dbf4
WinterCyan <wintercyan98@gmail.com>
2022-10-07 16:44:12 +0800

    ##

--- standalone/terminal_run.sh
+++ standalone/terminal_run.sh
@@ -4,16 +4,25 @@
 # except Exception:
 #     return []
 
+data_root="/Users/wintercyan/code-resource/treeseg/treeseg-resource/all"
+
 python3 split_merge_pipeline.py \
     --task preprocess_train \
-    --tif_dir /Users/wintercyan/LocalDocuments/treeseg-resource/full_area2/data \
-    --area_polygon_dir /Users/wintercyan/LocalDocuments/treeseg-resource/full_area2/data \
-    --area_range 2-3 \
-    --interm_png_dir /Users/wintercyan/LocalDocuments/treeseg-resource/full_area2/interm_png
+    --tif_dir $data_root/tif \
+    --area_polygon_dir $data_root/shp \
+    --area_range 3-4 \
+    --interm_png_dir $data_root/interm_png
+
+python3 split_merge_pipeline.py \
+    --task preprocess_train \
+    --tif_dir $data_root/tif \
+    --area_polygon_dir $data_root/shp \
+    --area_range 6-8 \
+    --interm_png_dir $data_root/interm_png
 
 python3 split_merge_pipeline.py \
     --task split_train \
-    --interm_png_dir /Users/wintercyan/LocalDocuments/treeseg-resource/full_area2/interm_png \
-    --sample_dir /Users/wintercyan/LocalDocuments/treeseg-resource/full_area2/sample \
+    --interm_png_dir $data_root/interm_png \
+    --sample_dir $data_root/sample_256 \
     --split_unit 256 \
     --norm_mode after_split
