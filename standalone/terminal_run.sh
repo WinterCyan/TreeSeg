a0654d7c577ad0c9c6e57d3769b336c0bc76f883
WinterCyan <wintercyan98@gmail.com>
2022-11-15 04:10:47 +0800

    new model inferenced

--- standalone/terminal_run.sh
+++ standalone/terminal_run.sh
@@ -51,22 +51,22 @@ data_root="/home/lenovo/treeseg-dataset/inference/all-views"
 #     --sample_dir $data_root/inference_sample_108 \
 #     --split_unit 108
 
-python3 split_merge_pipeline.py \
-    --task split_inference \
-    --tif_dir $data_root/tif \
-    --sample_dir $data_root/inference_sample_160 \
-    --split_unit 128
-
-# # -------------------------- inference --------------------------
 # python3 split_merge_pipeline.py \
-#     --task inference \
-#     --model_path /home/lenovo/code/TreeSeg/notebooks/saved_models/UNet/model-1115.h5 \
-#     --sample_dir $data_root/inference_sample_108 \
-#     --result_dir $data_root/inference_result_108 \
+#     --task split_inference \
+#     --tif_dir $data_root/tif \
+#     --sample_dir $data_root/inference_sample_128 \
+#     --split_unit 128
+
+# -------------------------- inference --------------------------
+python3 split_merge_pipeline.py \
+    --task inference \
+    --model_path /home/lenovo/code/TreeSeg/notebooks/saved_models/UNet/model-1115.h5 \
+    --sample_dir $data_root/inference_sample_128 \
+    --result_dir $data_root/inference_result_128 \
 
 # # -------------------------- merge --------------------------
 # python3 split_merge_pipeline.py \
 #     --task merge \
-#     --result_dir $data_root/inference_result_108 \
-#     --split_unit 108 \
-#     --merge_dir $data_root/merge_result_108 \
+#     --result_dir $data_root/inference_result_128 \
+#     --split_unit 128 \
+#     --merge_dir $data_root/merge_result_128 \
